b43ff8f88c6b32532990fc5f352dcdec82b00f5e
kamioigit <info@kamioi.com>
2026-06-03 19:11:17 -0400

    chore: bump iOS build 22, Android 17

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -17,7 +17,7 @@ module.exports = ({ config }) => {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.govirall.app',
-      buildNumber: '21',
+      buildNumber: '22',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
       },
@@ -29,7 +29,7 @@ module.exports = ({ config }) => {
       },
       package: 'com.govirall.app',
       softwareKeyboardLayoutMode: 'pan',
-      versionCode: 16,
+      versionCode: 17,
     },
     plugins: [
       'expo-router',
